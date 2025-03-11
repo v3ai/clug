80b8023b067e20add5ca619803b60321a5dd28ac
Rhett Applestone <rhettapples@googleemailservice.com>
2025-03-11 14:18:15 -0500

    Pushed with qcom: https://github.com/v3ai/qcom

--- components/header.js
+++ components/header.js
@@ -64,8 +64,10 @@ class Header extends HTMLElement {
   			<li><a href="index.html">Home</a></li>
   			<li><a href="contact.html">Contact</a></li>
   			<li><a href="about.html">About</a></li>
-   			<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdhasvr7teGdi8tPATjRKXqVJM7ZSmm0MaFO-0VTeUzTVvmMw/viewform?usp=sharing" target="_blank">Application</a></li>
    			<li><a href="members.html">Members</a></li>
+   			<li><a href="meetings.html">Meetings</a></li>
+   			<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdhasvr7teGdi8tPATjRKXqVJM7ZSmm0MaFO-0VTeUzTVvmMw/viewform?usp=sharing" target="_blank">Application</a></li>
+   			<li><a href="recs.html">Recs</a></li>
 		</ul>
 		<h2 style="text-align: center">Crimson Linux User's Group</h2>
 		<br>
