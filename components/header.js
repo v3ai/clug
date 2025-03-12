5813b0fc85e450391e2fb361e5cc4ea1cd47c248
Rhett Applestone <rhettapples@googleemailservice.com>
2025-03-12 02:24:34 -0500

    Pushed with qcom: https://github.com/v3ai/qcom

--- components/header.js
+++ components/header.js
@@ -5,61 +5,6 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <style>
-		body {
-			max-width: 45em;
-			margin: auto;
-			font-family: 'Courier New', monospace;
-			background-color: #000000;
-			color: white;
-		}
-		
-		.line {
-			border: dashed 0.1em white;
-		}
-		
-		.content {
-			margin-top: 2em;	
-			margin-bottom: 2em;	
-		}
-		
-		p {
-			padding-left: 3em;
-		}
-		
-		a {
-			color: #AE87FE;
-		}
-		
-		ul {
-  			list-style-type: none;
-  			margin: 0;
-  			padding: 0;
-  			overflow: hidden;
-  			background-color: #000000;
-		}
-		
-		li {
-  			float: left;
-		}
-		
-		li a {
-  			display: block;
-  			color: #AE87FE;
-  			text-align: center;
-  			padding: 14px 16px;
-  			text-decoration: none;
-  			border-style: solid;
-  			border-width: 0.07em;
-  			border-color: #000000;
-		}
-		 
-		li a:hover {
-			border-style: solid;
-			border-width: 0.07em;
-  			border-color: #AE87FE;
-		}
-      </style>
 		<ul>
   			<li><a href="index.html">Home</a></li>
   			<li><a href="contact.html">Contact</a></li>
